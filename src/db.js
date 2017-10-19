a4fe5e8a5f9d03b282496bd0a6a6c3dda141516a
Troy Wu <pightmp@gmail.com>
2017-10-19 15:47:20 +0800

    split /create into db

---
+++ src/db.js
@@ -0,0 +1,67 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+	host     : process.env.db_host,
+	user     : process.env.db_user,
+	password : process.env.db_password,
+	database : process.env.db_name
+})
+
+function start() {
+	connection.connect()
+}
+
+function end() {
+	connection.end()
+}
+
+function query(sql, params,callback) {
+	connection.query(sql, params, function(err, result) {
+		if (err) {
+			throw err
+		}
+		else {
+			callback(result)
+		}
+	})
+}
+
+function selectAllFromRes(params, callback) {
+	let sql = 'SELECT * FROM restaurant;'
+	query(sql, params, callback)
+}
+
+function selectMenuByResId(params, callback) {
+	let sql = 'SELECT item.*, item_type.name type_name FROM item \
+						 INNER JOIN item_type ON item.type_id = item_type.id \
+						 WHERE restaurant_id = ?;'
+	query(sql, params, callback)
+}
+
+function selectResNameByResId(params, callback) {
+	let sql = 'SELECT restaurant.name FROM restaurant WHERE id = ?'
+	query(sql, params, callback)
+}
+
+function selectHisByAccId(params, callback) {
+	let sql = 'SELECT e.id, r.name, e.start_time, e.end_time \
+	           FROM event e, restaurant r \
+	           WHERE e.account_id = ?;'
+	query(sql, params, callback)
+}
+
+function createEvent(params, callback) {
+	let sql = 'INSERT INTO event (restaurant_id, start_time, end_time, account_id) \
+						 VALUES (?, ?, ?, ?);'
+	query(sql, params, callback)
+}
+
+module.exports = {
+	start,
+	end,
+	selectAllFromRes,
+	selectMenuByResId,
+	selectResNameByResId,
+	selectHisByAccId,
+	createEvent
+}
