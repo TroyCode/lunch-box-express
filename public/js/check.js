76ea3fc57b94b2586fa31a69fbd953cc83165f72
yochen1325117 <chen1325117@gmail.com.tw>
2017-11-09 13:56:57 +0800

    新增check.js 修改create_menu.pug

---
+++ public/js/check.js
@@ -0,0 +1,8 @@
+function check_price_num(need_ckeck){
+    var price_check = /^[0-9]+$/
+    return price_check.test(need_ckeck);
+}
+function check_phone_num(phone_num){
+    let phone_check=/^[0-9]{10}$/;
+    return (phone_check.test(phone_num))
+}
