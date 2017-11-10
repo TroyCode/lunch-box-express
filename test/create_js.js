56ec6625db8ac5ad0e04bb3c952546d4a77acbac
yochen1325117 <chen1325117@gmail.com.tw>
2017-11-10 17:01:00 +0800

    create_js 初test

--- test/create_js.js
+++ test/create_js.js
@@ -1,10 +1,8 @@
-const index = require('../public/javascript/main.js')
+const index = require('../public/js/create_js.js')
 const assert = require('chai').assert
-
+const time_check = /^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}T[0-9]{1,2}:[0-9]{1,2}/;
 describe('time_back', () => {
-		it('should pass', () => {
-			return request(server)
-			.get('/login')
-			.expect(200)
+		it('should faler', () => {
+			assert.isTrue(time_check.test(index.time_back()), 'time should be true')			
 		})
 })
