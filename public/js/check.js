8df937eeedb644dd5055c5ed35290066a93a6b4e
yochen1325117 <chen1325117@gmail.com.tw>
2017-11-13 13:48:06 +0800

    建立菜單.pug 更新

--- public/js/check.js
+++ public/js/check.js
@@ -3,6 +3,11 @@ function check_price_num(need_ckeck){
     return price_check.test(need_ckeck);
 }
 function check_phone_num(phone_num){
-    let phone_check=/^[0-9]{10}$/;
+    let phone_check=/^[0-9]{2}-[0-9]{8}$/;
     return (phone_check.test(phone_num))
+}
+function phone_press_key(num){
+    if(num.length==2){
+        document.getElementById('shop_phone').value=num+'-'
+    }
 }
