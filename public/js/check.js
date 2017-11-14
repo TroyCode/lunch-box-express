e2b1926a87f06106b5b3b20993e6ea540f26f287
yochen1325117 <chen1325117@gmail.com.tw>
2017-11-14 09:55:40 +0800

    check.js req:檢查中文 create_menu:修改

--- public/js/check.js
+++ public/js/check.js
@@ -6,12 +6,19 @@ function check_phone_num(phone_num){
     let phone_check=/^[0-9]{2}-[0-9]{8}$/;
     return (phone_check.test(phone_num))
 }
-function phone_press_key(num){
+function phone_up_key(num){
     var num_check = /[0-9]{2}/
     if(num.length==2 && num_check.test(num)==true){
         document.getElementById('shop_phone').value=num+'-'
     }
 }
+
+function check_word(word){
+    let word_check = /^[a-zA-Z0-9\u4e00-\u9fa5]$/g;
+    return word_check.test(word)
+}
+
+
 
 module.exports = {
     check_price_num,
