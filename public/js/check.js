fafb70cf5336b1b714a48f8851b9eccdc533be28
yochen1325117 <chen1325117@gmail.com.tw>
2017-11-14 11:43:29 +0800

    新增 檢查input 顏色 輸入,提醒視窗

--- public/js/check.js
+++ public/js/check.js
@@ -13,9 +13,42 @@ function phone_up_key(num){
     }
 }
 
+//檢查 中間空格大小寫英文數字中文
 function check_word(word){
-    let word_check = /^[a-zA-Z0-9\u4e00-\u9fa5]$/g;
+    let word_check = /^[\sa-zA-Z0-9\u4e00-\u9fa5\-]+$/g;
     return word_check.test(word)
+}
+
+function check_input(){
+    let check_input = document.querySelectorAll('input')
+    var max = 0;
+    for(var i = 0 ;i<check_input.length;i++){
+        //input 檢查顏色
+        if(check_input[i].style.color=='red')
+        {
+            max++
+        }
+        //每個input.value去頭去尾空格檢查
+        if( check_word( check_input[i].value.trim() ) !=true )
+        {
+            max++
+        }
+    }
+    return max
+}
+function check_function(){
+    let check_box= document.getElementsByClassName('check_box');
+    var count = check_input();
+    if(check_input()>0){
+        check_box[0].style.visibility='visible'
+    }else if(check_input()<=0){
+        check_box[0].style.visibility='hidden'
+    }
+    console.log(count)
+}
+
+window.onload=function(){
+    var time_to_check = setInterval(check_function, 2000)
 }
 
 
